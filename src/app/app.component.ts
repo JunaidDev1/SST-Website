6ed22953b9354628bda9b2303a05b1c994427aa7
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2022-01-14 18:42:07 +0500

    Project created

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'sst-website';
+
+  constructor() {
+
+  }
+
+  onActivate(event) {
+    window.scroll(0, 0);
+  }
+
+}
