f138c795bdc46ae75391db10bb8f0c648519bd76
Junaid Iqbal <51758839+JunaidDev1@users.noreply.github.com>
2022-05-11 02:03:53 +0500

    V1 Published

--- src/app/template/testimonial/testimonial.component.ts
+++ src/app/template/testimonial/testimonial.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestimonialComponent implements OnInit {
 
+  reviews: any = [
+    {
+      reviewerImage: './assets/img/feedback/omega.png',
+      reviewerName: 'Justus Che',
+      review: 'Great job, beyond expectation. They did their job right on point. Planning another project with them already.',
+      reviewerCompany: 'Omega Soft - USA'
+    },
+    {
+      reviewerImage: './assets/img/feedback/bilal.png',
+      reviewerName: 'Bilal Lagrioui',
+      review: 'Great communicators, Excellent in what they do, timely delivery. They are the best developers I have woked with this far.',
+      reviewerCompany: 'High Developers - Morocco'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
